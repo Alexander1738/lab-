4fe6a5d80e89b8de11a920c6c9932e9becd718cc
Alex <konalexvad@gmail.com>
2026-04-17 23:49:11 +0300

    Add files via upload

---
+++ string 3/string 3/string 3.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+#include <map>
+#include <string>
+#include <unordered_map>
+
+std::unordered_map<std::string, int> count_characters(std::string& s) {
+    std::unordered_map<std::string, int> counts;
+
+    for (char c : s) {
+        std::string char_str = "";
+        char_str += c;
+
+        counts[char_str]++;
+    }
+
+    return counts;
+}
+
+int main() {
+    std::string s;
+    std::getline(std::cin, s);
+
+    std::unordered_map<std::string, int> res = count_characters(s);
+    std::map<std::string, int> ordered_res(res.begin(), res.end());
+    for (auto const& pair : ordered_res) {
+        std::cout << pair.first << ' ' << pair.second << '\n';
+    }
+
+    return 0;
+}
