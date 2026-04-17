3e72ddc5d1b5732fe75c781927fac4ed9c7b55c7
Alex <konalexvad@gmail.com>
2026-04-17 23:37:38 +0300

    Add files via upload

---
+++ string 1/string 1/string 1.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+#include <string>
+
+int compare_strings(std::string& s1, std::string& s2) {
+    if (s1 < s2) {
+        return -1; 
+    }
+    if (s1 > s2) {
+        return 1;  
+    }
+    return 0;      
+}
+int main() {
+    std::string s1;
+    std::getline(std::cin, s1);
+    std::string s2;
+    std::getline(std::cin, s2);
+    int res = compare_strings(s1, s2);
+    std::cout << res << '\n';
+    return 0;
+}
